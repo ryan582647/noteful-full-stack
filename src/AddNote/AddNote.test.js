1c0800c014c031c1c7086ec96dad944292363cf7
computershawn <shontek@gmail.com>
2019-05-30 19:50:50 -0700

    Go back to non-complete version

--- src/AddNote/AddNote.test.js
+++ src/AddNote/AddNote.test.js
@@ -24,10 +24,8 @@ describe(`AddNote component`, () => {
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 
-  // enzyme doesn't support React.createContext
-  it.skip('renders the select options from folders', () => {
-    const context = { folders: stubFolders }
-    const select = shallow(<AddNote />, context)
+  it('renders the select options from folders', () => {
+    const select = shallow(<AddNote folders={stubFolders} />)
       .find('#note-folder-select')
     expect(toJson(select)).toMatchSnapshot()
   })
--- src/App/App.js
+++ src/App/App.js
@@ -7,8 +7,8 @@ import NoteListMain from '../NoteListMain/NoteListMain'
 import NotePageMain from '../NotePageMain/NotePageMain'
 import AddFolder from '../AddFolder/AddFolder'
 import AddNote from '../AddNote/AddNote'
-import ApiContext from '../ApiContext'
-import config from '../config'
+import dummyStore from '../dummy-store'
+import { getNotesForFolder, findNote, findFolder } from '../notes-helpers'
 import './App.css'
 
 class App extends Component {
@@ -18,54 +18,12 @@ class App extends Component {
   };
 
   componentDidMount() {
-    Promise.all([
-      fetch(`${config.API_ENDPOINT}/notes`),
-      fetch(`${config.API_ENDPOINT}/folders`)
-    ])
-      .then(([notesRes, foldersRes]) => {
-        if (!notesRes.ok)
-          return notesRes.json().then(e => Promise.reject(e))
-        if (!foldersRes.ok)
-          return foldersRes.json().then(e => Promise.reject(e))
-
-        return Promise.all([
-          notesRes.json(),
-          foldersRes.json(),
-        ])
-      })
-      .then(([notes, folders]) => {
-        this.setState({ notes, folders })
-      })
-      .catch(error => {
-        console.error({ error })
-      })
-  }
-
-  handleAddFolder = folder => {
-    this.setState({
-      folders: [
-        ...this.state.folders,
-        folder
-      ]
-    })
-  }
-
-  handleAddNote = note => {
-    this.setState({
-      notes: [
-        ...this.state.notes,
-        note
-      ]
-    })
-  }
-
-  handleDeleteNote = noteId => {
-    this.setState({
-      notes: this.state.notes.filter(note => note.id !== noteId)
-    })
+    // fake date loading from API call
+    setTimeout(() => this.setState(dummyStore), 600)
   }
 
   renderNavRoutes() {
+    const { notes, folders } = this.state
     return (
       <>
         {['/', '/folder/:folderId'].map(path =>
@@ -73,12 +31,28 @@ class App extends Component {
             exact
             key={path}
             path={path}
-            component={NoteListNav}
+            render={routeProps =>
+              <NoteListNav
+                folders={folders}
+                notes={notes}
+                {...routeProps}
+              />
+            }
           />
         )}
         <Route
           path='/note/:noteId'
-          component={NotePageNav}
+          render={routeProps => {
+            const { noteId } = routeProps.match.params
+            const note = findNote(notes, noteId) || {}
+            const folder = findFolder(folders, note.folderId)
+            return (
+              <NotePageNav
+                {...routeProps}
+                folder={folder}
+              />
+            )
+          }}
         />
         <Route
           path='/add-folder'
@@ -93,6 +67,7 @@ class App extends Component {
   }
 
   renderMainRoutes() {
+    const { notes, folders } = this.state
     return (
       <>
         {['/', '/folder/:folderId'].map(path =>
@@ -100,12 +75,30 @@ class App extends Component {
             exact
             key={path}
             path={path}
-            component={NoteListMain}
+            render={routeProps => {
+              const { folderId } = routeProps.match.params
+              const notesForFolder = getNotesForFolder(notes, folderId)
+              return (
+                <NoteListMain
+                  {...routeProps}
+                  notes={notesForFolder}
+                />
+              )
+            }}
           />
         )}
         <Route
           path='/note/:noteId'
-          component={NotePageMain}
+          render={routeProps => {
+            const { noteId } = routeProps.match.params
+            const note = findNote(notes, noteId)
+            return (
+              <NotePageMain
+                {...routeProps}
+                note={note}
+              />
+            )
+          }}
         />
         <Route
           path='/add-folder'
@@ -113,38 +106,36 @@ class App extends Component {
         />
         <Route
           path='/add-note'
-          component={AddNote}
+          render={routeProps => {
+            return (
+              <AddNote
+                {...routeProps}
+                folders={folders}
+              />
+            )
+          }}
         />
       </>
     )
   }
 
   render() {
-    const value = {
-      notes: this.state.notes,
-      folders: this.state.folders,
-      addFolder: this.handleAddFolder,
-      addNote: this.handleAddNote,
-      deleteNote: this.handleDeleteNote,
-    }
     return (
-      <ApiContext.Provider value={value}>
-        <div className='App'>
-          <nav className='App__nav'>
-            {this.renderNavRoutes()}
-          </nav>
-          <header className='App__header'>
-            <h1>
-              <Link to='/'>Noteful</Link>
-              {' '}
-              <FontAwesomeIcon icon='check-double' />
-            </h1>
-          </header>
-          <main className='App__main'>
-            {this.renderMainRoutes()}
-          </main>
-        </div>
-      </ApiContext.Provider>
+      <div className='App'>
+        <nav className='App__nav'>
+          {this.renderNavRoutes()}
+        </nav>
+        <header className='App__header'>
+          <h1>
+            <Link to='/'>Noteful</Link>
+            {' '}
+            <FontAwesomeIcon icon='check-double' />
+          </h1>
+        </header>
+        <main className='App__main'>
+          {this.renderMainRoutes()}
+        </main>
+      </div>
     )
   }
 }
