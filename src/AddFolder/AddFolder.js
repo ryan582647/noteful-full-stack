8df4e9aa4039111af680136655b6f049ded805ba
computershawn <shontek@gmail.com>
2019-05-31 00:10:08 -0700

    Add and delete functionality...
    works for Notes and Folders.TODO: Prevent user from submitting a blank note;
    Define an error boundary component whatever the hell that is. Define PropType validation for components that receive props from a parent

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react'
 import NotefulForm from '../NotefulForm/NotefulForm'
+import NotesFoldersContext from '../NotesFoldersContext'
 import './AddFolder.css'
 
 export default class AddFolder extends Component {
+  static defaultProps = {
+    history: {
+      push: () => {}
+    },
+  }
+  static contextType = NotesFoldersContext;
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const folder = {
+      name: e.target['folder-name'].value
+    }
+    fetch(`http://localhost:9090/folders/`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(folder),
+    })
+      .then(res => {
+        if (!res.ok)
+          return res.json().then(e => Promise.reject(e))
+        return res.json()
+      })
+      .then(folder => {
+        this.context.addFolder(folder)
+        this.props.history.push(`/folder/${folder.id}`)
+      })
+      .catch(error => {
+        console.error({ error })
+      })
+  }
+
   render() {
     return (
       <section className='AddFolder'>
         <h2>Create a folder</h2>
-        <NotefulForm history={this.props.history}>
+        <NotefulForm onSubmit={this.handleSubmit}>
           <div className='field'>
             <label htmlFor='folder-name-input'>
               Name
